dcb1fcf1435dd25b4ed0ecaebc5ac5f15f77ed91
shangzewen <792906029@qq.com>
2020-03-14 19:26:54 +0800

    feat: finished task7

--- shellPrograms/shellDaemonize_code.c
+++ shellPrograms/shellDaemonize_code.c
@@ -13,7 +13,7 @@
 #include "shellPrograms.h"
 
 //TODO: change to appropriate path
-char *path = "/Users/natalie_agus/Dropbox/50.005 Computer System Engineering/2020/PA1 Makeshell Daemon/PA1/logfile_test.txt";
+char *path = "/Users/huaihaizi/Desktop/logfile_test.txt";
 
 /*This function summons a daemon process out of the current process*/
 static int create_daemon()
@@ -31,6 +31,31 @@ static int create_daemon()
     // 7. Change working directory to root
     // 8. Close all open file descriptors using sysconf(_SC_OPEN_MAX) and redirect fd 0,1,2 to /dev/null
     // 9. Return to main
+    pid_t pid_1 = fork();
+    if(pid_1==0){
+        setsid();
+        signal(SIGCHLD, SIG_IGN);
+        signal(SIGHUP, SIG_IGN);
+        pid_t pid_2 = fork();
+        if(pid_2==0){
+            umask(0);
+            chdir("/Users/huaihaizi");
+               int x;
+            for (x = sysconf(_SC_OPEN_MAX); x>=0; x--)
+            {
+                close (x);
+            }
+            stdin = open("/dev/null", O_RDWR);
+            stdout = dup(0);
+            stderr = dup(0);
+        }else{
+            exit(1);
+        }
+
+    }else{
+        exit(1);
+    }
+
 
     return 1;
 }
